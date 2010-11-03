64c0cf568d491844e79ba74a32a1a4773ff5f14e
Chris Drackett <chris@chrisdrackett.com>
2010-11-02 18:45:06 -0700

    adding support for webkit translate3d for speed (code is a bit tied to bigheartsfund.org right now and needs cleanup)

--- Source/ElementStack.js
+++ Source/ElementStack.js
@@ -5,6 +5,7 @@ decription: ElementStacks stacks elements/images on top of each other with a fun
 license: MIT-style license.
 authors:
  - Oskar Krawczyk (http://nouincolor.com/)
+ - Chris Drackett (http://chrisdrackett.com/)
 requires: 
   core:1.2.3: 
   - Class.Extras
@@ -20,12 +21,13 @@ provides: [ElementStacks]
 */
 
 var ElementStacks = new Class({
-	
+
     Implements: [Options],
     
     options: {
-        rotationDeg: 20,
-        delay: 40,
+        rotationDeg: 12,
+        delay: 30,
+        margin: 6,
         duration: 900,
         transition: 'back:out'
     },
@@ -35,28 +37,31 @@ var ElementStacks = new Class({
         this.pos, this.collapsed, this.wrapper = wrapper, this.stackItems = selector
         
         this.setDefaults();
+        
+        this.reStack(this.stackItems, 'in', this.stackItems[$random(0, 8)]);
+        this.collapsed = true;
     },
     
     setDefaults: function(){
         this.stackItems.each(function(stackItem){
             this.pos = stackItem.getPosition(this.wrapper);
-
+            
             stackItem.store('default:coords', this.pos);
-        
-            stackItem.set('styles', {
-                top: this.pos.y,
-                left: this.pos.x
-            });
-    
+            stackItem.store('default:size', stackItem.getSize());
+            this.default_size = stackItem.getSize();
+            
             stackItem.set('morph', {
                 transition: this.options.transition,
                 duration: this.options.duration
             });
-
-            // MooTools bug (?)
-            (function(){
-                this.setStyle('position', 'absolute');
-            }).delay(1, stackItem);
+            
+            if ($('facts')) {
+                $('facts').set('morph', {
+                    transition: this.options.transition,
+                    duration: this.options.duration
+                });
+            }
+            
         }, this);
         
         this.attachActions();
@@ -64,43 +69,83 @@ var ElementStacks = new Class({
     
     reStack: function(els, mode, altEl){
         var that = this;
+        
+        if (mode == 'in') {
+            var x_pos = 0;
+            var y_pos = 0;
+        } else {
+            var x_pos = -this.default_size.x;
+            var y_pos = -this.default_size.y;
+        };
+        var count = 1;
+        
+        if (mode == 'in'){
+            altEl.setStyle('z-index', 100);
+        } else {
+            (function(){
+                els.setStyle('z-index', 10);
+            }).delay(that.options.delay * els.length);
+        }
+        
         els.each(function(stackItem, i){
             (function(){
                 var el = $pick(altEl, this);
+                
                 var rand = (mode === 'in' ? $random(-that.options.rotationDeg, that.options.rotationDeg) : 0);
+                var scale = (mode === 'in' ? 1 : 0.5);
                 
-                this.set('styles', {
-                    '-webkit-transform': 'rotate(' + rand + 'deg)',
-                    '-moz-transform': 'rotate(' + rand + 'deg)'
-                });
-                
-                this.morph({
-                    top: el.retrieve('default:coords').y + rand,
-                    left: el.retrieve('default:coords').x + rand
-                });
-                
-                if (mode === 'in'){
-                    el.setStyle('z-index', 100);
+                if (Browser.safari || Browser.chrome) {
+                    this.set('styles', {
+                        '-webkit-transform': 'scale3d(' + scale + ', ' + scale + ', ' + scale + ') translate3d(' + (x_pos + rand) +'px, ' + (y_pos + rand) +'px, 0px) rotateZ(' + rand + 'deg)'
+                    });
+                    
+                    if ($('facts') && mode == 'in' && this == el) {
+                        $('facts').set('styles', {
+                            '-webkit-transform': 'translate3d(' + -i * 950 + 'px, 0px, 0px)'
+                        });
+                    }
+                    
                 } else {
-                    (function(){
-                        els.setStyle('z-index', 10);
-                    }).delay(that.options.delay * (els.length * 2));
+                    this.set('styles', {
+                        '-moz-transform': 'rotate(' + rand + 'deg)'
+                    });
+                    
+                    this.morph({
+                        top: y_pos + rand,
+                        left: x_pos + rand
+                    });
+                    
+                    if ($('facts') && mode == 'in' && this == el) {
+                        $('facts').morph({
+                            left: -i * 950
+                        });
+                    }
+                    
+                }
+                
+                if (mode != 'in') {
+                    x_pos = x_pos + stackItem.retrieve('default:size').x + that.options.margin;
+                    
+                    if (count == 3) {
+                        count = 0;
+                        x_pos = -stackItem.retrieve('default:size').x;
+                        y_pos = y_pos + stackItem.retrieve('default:size').y + that.options.margin;
+                    }
+                    count++
                 }
             }).delay(that.options.delay * i, stackItem);
         });
     },
 
     attachActions: function(){
-        
         this.stackItems.addEvents({
             click: function(e){
-
                 if (this.collapsed){
                     this.reStack(this.stackItems);
                     this.collapsed = false;
                 } else {
                     var target = $(e.target);
-
+                    
                     if (target.retrieve('default:coords')){
                        this.reStack(this.stackItems, 'in', target);
                        this.collapsed = true;
